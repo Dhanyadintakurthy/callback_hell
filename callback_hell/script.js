672f2d0263445bc68efbb0d02953749e9476c678
Dhanyadintakurthy <72096425+Dhanyadintakurthy@users.noreply.github.com>
2021-10-16 23:10:30 +0530

    Add files via upload

---
+++ callback_hell/script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.querySelector("h1").innerHTML = "10";
+    setTimeout(()=>{
+      document.querySelector("h1").innerHTML = "9";
+      setTimeout(()=>{
+        document.querySelector("h1").innerHTML = "8";
+        setTimeout(()=>{
+          document.querySelector("h1").innerHTML = "7";
+          setTimeout(()=>{
+            document.querySelector("h1").innerHTML = "6";
+            setTimeout(()=>{
+              document.querySelector("h1").innerHTML = "5";
+              setTimeout(()=>{
+                document.querySelector("h1").innerHTML = "4";
+                setTimeout(()=>{
+                  document.querySelector("h1").innerHTML = "3";
+                  setTimeout(()=>{
+                    document.querySelector("h1").innerHTML = "2";
+                    setTimeout(()=>{
+                      document.querySelector("h1").innerHTML = "1";
+                      setTimeout(()=>{
+                        document.querySelector("h1").innerHTML = "happy independance day";
+                      },1000*1);
+                    },1000*1);
+                  },1000*1);
+                },1000*1);
+              },1000*1);
+            },1000*1);
+          },1000*1);
+        },1000*1);
+      },1000*1);
+    },1000*1);
+  },1000*1);
